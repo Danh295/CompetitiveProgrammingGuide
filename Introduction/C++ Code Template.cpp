38f300d21231ee99e9538f43ed18fd7c93c81b99
danh295 <hudanny295@gmail.com>
2021-12-26 17:15:09 -0500

    Update C++ Code Template.cpp

--- Introduction/C++ Code Template.cpp
+++ Introduction/C++ Code Template.cpp
@@ -1,19 +1,14 @@
 // This is a typical code template for competitive programming in C++
 
-#include <bits/stdc++.h>
-// "#include" is a feature of the g++ compiler, and in this case, it is used
-// to include the entire standard library, just simplicity sake for the programmer
+#include <bits/stdc++.h> // "#include" is a feature of the g++ compiler, and in this case, it is used to include the entire standard library, just simplicity sake for the programmer
 
-using namespace std;
-// "using namespace std" allows classes and functions from the standard library to be used
-// directly in the code; it just saves some time for the programmer. Without this line, we would
-// have to write "std::cout", but with this line, we can just write "cout"
+using namespace std; // "using namespace std" allows classes and functions from the standard library to be used directly in the code; it just saves some time for the programmer
+                     // Without this line, we would have to write "std::cout", but with this line, we can just write "cout"
 
-int main() {
-    // solution written here
-        // Competitive programming does not usually require OOP, thus most
-        // of the code is implemented in the main function
+int main() { // Competitive programming does not usually require OOP, thus most of the code is implemented in the main function
     
+    // solution 
+
     return 0; //  this line is only necessary for C programs, but including it in C++ doesn't harm the program
 }
 
