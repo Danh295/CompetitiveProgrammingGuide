f6bc7f27c0093843c177f0df0e4e466b890c3986
danh295 <hudanny295@gmail.com>
2021-12-30 21:48:09 -0500

    Update merge_sort.cpp
    Finally resolved the issue, the culprit was a semi-colon, wasted my entire day looking for it

--- Sorting/merge_sort.cpp
+++ Sorting/merge_sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,20 +7,20 @@ void merge(int arr[], int l, int mid, int h)
 {
     int i, j, k; // iterator variables
 
-    int n1 = mid - l + 1; // size of lower temp arr
-    int n2 = h - mid; // size of higher temp arr
-
+    int n1 = mid - l + 1; // size of lower temp array
+    int n2 = h - mid; // size of higher temp array
+    
     int templ[n1], temph[n2]; // temp array
-
+    
     // copy data to the split arrays
     for (int i = 0; i < n1; i++)
         templ[i] = arr[l + i];
-    for (int j = 0; j < n2; j++);
+    for (int j = 0; j < n2; j++)
         temph[j] = arr[mid + 1 + j];
 
     i = 0; // initial index of first subarray
     j = 0; // initial index of second subarray
-    k = l; // initial index of merged subarray
+    k = l; // initial index fo merged subarray
 
     // sort & merge elements through iteration
     while (i < n1 && j < n2) 
@@ -54,9 +55,9 @@ void merge(int arr[], int l, int mid, int h)
 
 void mergeSort(int arr[], int l, int h) 
 {
-    if (l < h) // recursive execute until subarr only has 1 elem
+    if (l < h) // recursive execute until subarray only has 1 element
     { 
-        int mid = (l + h) / 2;
+        int mid = l + (h - l) / 2;
 
         mergeSort(arr, l, mid);
         mergeSort(arr, mid + 1, h);
@@ -65,26 +66,29 @@ void mergeSort(int arr[], int l, int h)
     }
 }
 
-int main() {
+int main() 
+{
+    // INIT
     ios::sync_with_stdio(0);
 
     // INPUT
-    int arr[] = {12, 11, 13, 5, 6, 7};
+        // 12, 11, 13, 7, 6, 7
+    int arr[6] = {12, 11, 13, 7, 6, 7}; // constant values; can be rewritten for user input
     int arrSize = sizeof(arr) / sizeof(arr[0]);
 
-    for(int i = 0; i < arrSize; i++) {
+    cout<<"Before sorting: ";
+    for(int i = 0; i < arrSize; i++) 
         cout<<arr[i]<<" ";
-    }
     cout<<"\n";
-
-    // PROCESS - call mergesort
+    
+    // PROGRESS
     mergeSort(arr, 0, arrSize - 1);
-
-    // OUPTUT
-        // int arr[5] = {1,2,4,5,7};
-    for(int i = 0; i < arrSize; i++) {
+   
+   // OUTPUT
+    // 5, 6, 7, 11, 12, 13
+   cout<<"After sorting: ";
+    for(int i = 0; i < arrSize; i++) 
         cout<<arr[i]<<" ";
-    }
     cout<<"\n";
 
 }
