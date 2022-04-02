b72ac472706f5ae925e54b443d18e3179f0c46c9
danh295 <hudanny295@gmail.com>
2022-04-01 23:51:26 -0400

    Update bubble_sort.cpp
    Minor formatting updates

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -16,11 +16,11 @@ int main(){
     cout << "size "<< n;
 
     // PROCESS (Bubble Sort algorithm)
-    for (int i=0; i<n-1; i++){          // # of passes = length of list - 1
-        for (int j=0; j<n-1-i; j++){    // 1 less comparison per pass (optimizes the algorithm)
+    for (int i=0; i<n-1; i++) {          // # of passes = length of list - 1
+        for (int j=0; j<n-1-i; j++) {    // 1 less comparison per pass (optimizes the algorithm)
 
             // compare adjacent items & swap if necessary
-            if (arr[j]>arr[j+1] && arr[i]!=arr[j]){
+            if (arr[j]>arr[j+1] && arr[i]!=arr[j]) {
                 cout << "Compared values: " << arr[j] << ", " << arr[j+1] <<"\n";
                 swap(arr[j], arr[j+1]);
                 cout << "New values: " << arr[j] << ", " << arr[j+1] << "\n";
